bab7c311c945317a384a14d8f6afd16b3883d1a7
Laila Alfayyadh <l_alfayyadh@hotmail.com>
2017-03-29 14:46:05 +0300

    remove unwanted images, add local ara font, apply Maher changes, minor to index, css and js

--- js/custom.js
+++ js/custom.js
@@ -5,43 +5,40 @@ $(document).ready(function () {
         // jQuery('.pulse').delay(500).fadeOut();
         // jQuery('.preloader').delay(1000).fadeOut('slow');
 
+
+        //Wareef curtain roll
           jQuery('#boxOne').delay(2000).animate({top: '100vh'}, 3000);
           jQuery('#boxTwo').delay(2000).animate({bottom: '100vh'}, 3000);
           jQuery('#boxOne').fadeOut();
           
-                
-
+               
     });
+
     $(window).resize(function () {
         $(".navbar-collapse").css({maxHeight: $(window).height() - $(".navbar-header").height() + "px"});
     });
+
     //sticky navbar
-    $(".navbar-sticky").sticky({topSpacing: 0});
+    $(".navbar-sticky").sticky({ topSpacing: 0 });
+
     //menu on hover
     $('.js-activated').dropdownHover({
         instantlyCloseOthers: false,
         delay: 0
     }).dropdown();
 
-    //owl arrows slider
-    $(".owl-arrows-slider").owlCarousel({
-    loop:true,
-    margin:10,
-    nav:false,
-    responsive:{
-        0:{
-            items:1
-        },
-        600:{
-            items:3
-        },
-        1000:{
-            items:3
+    //smooth scroll
+    $('a[href*="#"]:not([href="#"])').click(function () {
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            if (target.length) {
+                $('html, body').animate({
+                    scrollTop: target.offset().top - 70
+                }, 1000);
+                return false;
+            }
         }
-    }
-});
-    $('.counter-digit').counterUp({
-        delay: 10,
-        time: 5000
     });
+
 });
